2ae9dd22de0ffecbfe2c0e393e2a347d55d8c21a
mblanc <blanc.matthieu@gmail.com>
2013-05-06 18:38:22 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+  wine = require('./routes/wines'),
+  http = require('http');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/wines', wine.findAll);
+app.get('/wines/:id', wine.findById);
+app.post('/wines', wine.addWine);
+app.put('/wines/:id', wine.updateWine);
+app.delete('/wines/:id', wine.deleteWine);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
